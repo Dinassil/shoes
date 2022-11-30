f51252c5c33ee8668c39cbe4d854e43a28e9b616
Dinassil <Dinassil@bk.ru>
2022-11-30 11:14:15 +0600

    dd

---
+++ func5_provider.php
@@ -0,0 +1,43 @@
+<?php
+include 'config.php';
+
+$name = $_POST['name'];
+
+// Create
+
+if (isset($_POST['submit'])) {
+	$sql = ("INSERT INTO `provider`(`name`) VALUES(?)");
+	$query = $pdo->prepare($sql);
+	$query->execute([$name]);
+	$success = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Данные успешно отправлены!</strong> Вы можете закрыть это сообщение.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+	
+}
+
+// Read
+
+$sql = $pdo->prepare("SELECT * FROM `provider`");
+$sql->execute();
+$result = $sql->fetchAll();
+
+// Update
+$edit_name = $_POST['edit_name'];
+$get_id = $_GET['id'];
+if (isset($_POST['edit-submit'])) {
+	$sqll = "UPDATE provider SET name=? WHERE id=?";
+	$querys = $pdo->prepare($sqll);
+	$querys->execute([$edit_name, $get_id]);
+	header('Location: '. $_SERVER['HTTP_REFERER']);
+}
+
+// DELETE
+if (isset($_POST['delete_submit'])) {
+	$sql = "DELETE FROM provider WHERE id=?";
+	$query = $pdo->prepare($sql);
+	$query->execute([$get_id]);
+	header('Location: '. $_SERVER['HTTP_REFERER']);
+}
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-md navbar-dark
+    bg-dark">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">CRUD</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="category.php">Category</a>
+            <a class="dropdown-item" href="season.php">Season</a>
+            <a class="dropdown-item" href="name_of_product.php">Name_of_product</a>
+            <a class="dropdown-item" href="manufacturer.php">Manufacturer</a>
+            <a class="dropdown-item" href="provider.php">Provider</a>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#">Filter</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Catalog</a>
+          </div>
+        </li>
+        </ul>
+    </div>
+    <div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto" href="#">Магазин обуви</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Корзина</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Личный кабинет</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+<div class="card border-primary mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Primary card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-secondary mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Secondary card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-success mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Success card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-danger mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Danger card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-warning mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Warning card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-info mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Info card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-light mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Light card title</h4>
+    <p class="card-text">Some quick example text to  build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card border-dark mb-3 m-5" style="max-width: 20rem; float:left">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Dark card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
